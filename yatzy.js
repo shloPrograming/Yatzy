250f026d0fac55b38f17ff9d2112ec4804d56347
eaashlo <eaashlo@students.eaaa.dk>
2024-02-29 14:07:15 +0100

    We can now roll the dice

--- yatzy.js
+++ yatzy.js
@@ -1,5 +1,55 @@
+let diceHeld = [false, false, false, false, false];
+let diceValues = [0, 0, 0, 0, 0];
+let turn = 0;
 
-let rollButton = document.getElementById('rollButton');
-let turn = document.getElementById('turn');
+let rollButton = document.getElementById("rollButton");
 
-function rollDice() {return Math.floor(Math.random() * 6) + 1;}
+rollButton.onclick = () => buttonRoll();
+
+let dice = [document.getElementById('die1'), document.getElementById('die2'), document.getElementById('die3'), 
+document.getElementById('die4'), document.getElementById('die5')];
+
+
+function buttonRoll(){
+    rollDice();
+    updateDice();
+    console.log(diceValues);
+    turn++;
+    if (turn == 3){
+        rollButton.disabled = true;
+    }
+}
+
+function rollDice(){
+    for (i = 0; i < dice.length; i++){
+        if (!diceHeld[i]){
+            diceValues[i] = Math.floor(Math.random() * 6) + 1;
+        }
+    }
+}
+
+function updateDice(){
+    for (i = 0; i < dice.length; i++){
+        switch(diceValues[i]){
+            case 1:
+                dice[i].src = "dice-six-faces-one.png";
+                break;
+            case 2:
+                dice[i].src = "dice-six-faces-two.png";
+                break;
+            case 3:
+                dice[i].src = "dice-six-faces-three.png";
+                break;
+            case 4:
+                dice[i].src = "dice-six-faces-four.png";
+                break;
+            case 5:
+                dice[i].src = "dice-six-faces-five.png";
+                break;
+            case 6:
+                dice[i].src = "dice-six-faces-six.png";
+                break;
+        }
+    }
+}
+
