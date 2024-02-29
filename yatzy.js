658af0a2310fff4cfc0d2bf50ea0d1ddc25b3c1b
eaashlo <eaashlo@students.eaaa.dk>
2024-02-29 15:31:12 +0100

    Holding dice added

--- yatzy.js
+++ yatzy.js
@@ -9,11 +9,19 @@ rollButton.onclick = () => buttonRoll();
 let dice = [document.getElementById('die1'), document.getElementById('die2'), document.getElementById('die3'), 
 document.getElementById('die4'), document.getElementById('die5')];
 
+for (let i = 0; i < dice.length; i++){
+    dice[i].addEventListener("click", function(){
+        if (turn != 0){
+            diceHeld[i] = !diceHeld[i];
+        }
+    });
+}
 
 function buttonRoll(){
     rollDice();
     updateDice();
     console.log(diceValues);
+    console.log(diceHeld);
     turn++;
     if (turn == 3){
         rollButton.disabled = true;
@@ -30,25 +38,27 @@ function rollDice(){
 
 function updateDice(){
     for (i = 0; i < dice.length; i++){
-        switch(diceValues[i]){
-            case 1:
-                dice[i].src = "dice-six-faces-one.png";
-                break;
-            case 2:
-                dice[i].src = "dice-six-faces-two.png";
-                break;
-            case 3:
-                dice[i].src = "dice-six-faces-three.png";
-                break;
-            case 4:
-                dice[i].src = "dice-six-faces-four.png";
-                break;
-            case 5:
-                dice[i].src = "dice-six-faces-five.png";
-                break;
-            case 6:
-                dice[i].src = "dice-six-faces-six.png";
-                break;
+        if (!diceHeld[i]){
+            switch(diceValues[i]){
+                case 1:
+                    dice[i].src = "dice-six-faces-one.png";
+                    break;
+                case 2:
+                    dice[i].src = "dice-six-faces-two.png";
+                    break;
+                case 3:
+                    dice[i].src = "dice-six-faces-three.png";
+                    break;
+                case 4:
+                    dice[i].src = "dice-six-faces-four.png";
+                    break;
+                case 5:
+                    dice[i].src = "dice-six-faces-five.png";
+                    break;
+                case 6:
+                    dice[i].src = "dice-six-faces-six.png";
+                    break;
+            }
         }
     }
 }
