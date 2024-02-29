705a5a2bce3ab065692fa382b55e4d05be61b41a
eaashlo <eaashlo@students.eaaa.dk>
2024-02-29 07:12:07 +0100

    first commit

---
+++ yatzy.js
@@ -0,0 +1,6 @@
+
+let rollButton = document.getElementById('rollButton');
+rollButton.innerHTML = 'Roll the dice!';
+rollButton.onmouseover = function() {
+  rollButton.style.backgroundColor = 'lightblue';
+}
